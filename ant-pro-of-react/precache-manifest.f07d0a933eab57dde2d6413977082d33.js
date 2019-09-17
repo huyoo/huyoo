b18e045baeb62640bcc465ef2a3f4bee4951db3d
hy <huyu_scls@163.com>
2019-09-17 16:30:39 +0800

    挂载 ant-pro-of-react0.2.0

---
+++ ant-pro-of-react/precache-manifest.f07d0a933eab57dde2d6413977082d33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ca129be7d56fdb89dc52847e645391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c0d95428d4bf7bc4949",
+    "url": "/static/css/2.bb6fde67.chunk.css"
+  },
+  {
+    "revision": "eec4ba004615bd07810d",
+    "url": "/static/css/main.56bb9304.chunk.css"
+  },
+  {
+    "revision": "6c0d95428d4bf7bc4949",
+    "url": "/static/js/2.e2305da0.chunk.js"
+  },
+  {
+    "revision": "eec4ba004615bd07810d",
+    "url": "/static/js/main.604a32d2.chunk.js"
+  },
+  {
+    "revision": "80b3fd64f2d5cb0f8021",
+    "url": "/static/js/runtime~main.41d490fe.js"
+  },
+  {
+    "revision": "ffe9256f6f1fb657eee2a3fc7b55d45d",
+    "url": "./static/media/logo.ffe9256f.svg"
+  }
+]);
