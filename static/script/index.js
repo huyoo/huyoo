ff18c51dce91b1a252b03fa7c6a7d76bfb38c225
hy <huyu_scls@163.com>
2019-11-14 13:53:45 +0800

    test

--- static/script/index.js
+++ static/script/index.js
@@ -46,31 +46,8 @@
 
     function init() {
         let banner = document.getElementsByClassName('banner');
-        banner[0].children[0].style.backgroundImage = `url("${window.location.host.includes('localhost') ? '/huyoo.github.io' : ''}/static/img/AlbertaThanksgiving_ZH-CN5899007960_1920x1080.bg.jpg")`;
+        banner[0].children[0].style.backgroundImage = `url("${window.location.host.includes('localhost') ? '/huyoo.github.io' : ''}/static/img/AlbertaThanksgiving_ZH-CN5899007960_1920x1080.jpg")`;
     }
-
-
-    // var date,
-    //     menuDate = document.getElementById("menuDate"),
-    //     menuTime = document.getElementById("menuTime");
-    // function menu() {
-    //     date = new Date();
-    //     menuDate.innerHTML = date.getFullYear().toString()+"/" +
-    //         (date.getMonth()+1)+ "/"+ date.getDate();
-    //     menuTime.innerHTML =  date.getHours().toString() +" : "+
-    //         date.getMinutes()+ " : "+ date.getSeconds();
-    // };
-    // // window.setInterval(menu,500);
-    //
-    // var menuMsc = document.getElementById('menuMsc');
-    // menuMsc.onclick = function () {
-    //     var status = document.getElementById('menuMscCont'),
-    //         targ = menuMsc.children[0].children[2],
-    //         targClass = targ.className;
-    //     targ.className = targClass.match(/fa-plus/) ?
-    //         targClass.replace('fa-plus','fa-minus') : targClass.replace('fa-minus','fa-plus');
-    //     status.style.display = status.style.display === 'block'? 'none': 'block';
-    // };
 })(window);
 
 
