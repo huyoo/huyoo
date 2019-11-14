91e120c638f16068e22284e35c6de0a5aa467c28
hy <huyu_scls@163.com>
2019-11-14 10:56:32 +0800

    update: 大图片加载优化 文件请求优化

--- static/script/index.js
+++ static/script/index.js
@@ -1,5 +1,5 @@
 (function (window) {
-    init();
+    window.onload = init;
 
     const MENU_LIST = ['Home', "Demo", "About"],
         container = document.getElementsByClassName('container')[0],
